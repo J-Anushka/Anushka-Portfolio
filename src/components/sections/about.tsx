328a724195639eed7e367c069a498f242c43403a
Anushka <anushkajais1001@gmail.com>
2025-07-31 15:26:55 +0000

    remove italic from I have

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -29,7 +29,7 @@ export default function About() {
       <div className="container mx-auto">
         <div className="text-center mb-12">
           <h2 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl">What have I done?</h2>
-          <p className="mt-4 text-2xl text-muted-foreground italic">
+          <p className="mt-4 text-2xl text-muted-foreground">
              <Typewriter text="I have...." delay={100} />
           </p>
         </div>
