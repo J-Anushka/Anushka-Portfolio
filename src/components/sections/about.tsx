279511941673d5f327af7acb0515ffdb7b7c621c
Anushka <anushkajais1001@gmail.com>
2025-07-31 17:22:49 +0000

    change the title of been the anchor to 'Been an Anchor' and cintent to '

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -49,8 +49,8 @@ const projectEntries = [
     hint: "conference diplomacy",
   },
   {
-    title: "Been the Anchor",
-    description: "Anchored a flagship event organized by our Entrepreneurship Club, setting the tone, engaging the audience, and ensuring the energy stayed high throughout the program.",
+    title: "Been an Anchor",
+    description: "Anchored events by setting the tone, engaging the audience, and ensuring the energy stayed high throughout the program.",
     image: "https://images.unsplash.com/photo-1682258370582-377d685156bc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxldmVudCUyMG1pY3xlbnwwfHx8fDE3NTM5ODE2NTZ8MA&ixlib=rb-4.1.0&q=80&w=1080",
     hint: "public speaking event",
   },
