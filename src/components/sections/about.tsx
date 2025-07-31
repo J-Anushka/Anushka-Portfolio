d03f14a0bcca0d1260bdb4d34503360ea72ed0e6
Anushka <anushkajais1001@gmail.com>
2025-07-31 15:31:41 +0000

    I have....

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -29,7 +29,7 @@ export default function About() {
         <div className="text-center mb-12">
           <h2 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl">What have I done?</h2>
           <p className="mt-4 text-2xl text-muted-foreground">
-             <Typewriter text="I have done...." delay={100} />
+             <Typewriter text="I have...." delay={100} />
           </p>
         </div>
       </div>
