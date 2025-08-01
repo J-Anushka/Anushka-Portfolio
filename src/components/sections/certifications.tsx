e195b4c3c2526584269f9cf3bfa9d7af01e6f52e
Anushka <anushkajais1001@gmail.com>
2025-08-01 12:26:43 +0000

    year is 2024

--- src/components/sections/certifications.tsx
+++ src/components/sections/certifications.tsx
@@ -29,16 +29,16 @@ const certificationsData = [
         link: "https://www.coursera.org/account/accomplishments/certificate/1I3HFBF95N04",
     },
     {
+        title: "Code Vipassana",
+        issuer: "Google Cloud",
+        date: "2024",
+        link: "https://www.linkedin.com/posts/anushkajaiswal1_reflecting-on-my-month-long-journey-with-activity-7213520342609186817-lw02?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD9yF5sBiwZPChl0LVJX9sj86SYfOuDErFQ"
+    },
+    {
         title: "The Science of Well-Being",
         issuer: "Yale University",
         date: "Sep 2023",
         link: "https://www.coursera.org/account/accomplishments/certificate/8UX2PRQJDV4C",
-    },
-    {
-        title: "Code Vipassana",
-        issuer: "Google Cloud",
-        date: "2023",
-        link: "https://www.linkedin.com/posts/anushkajaiswal1_reflecting-on-my-month-long-journey-with-activity-7213520342609186817-lw02?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD9yF5sBiwZPChl0LVJX9sj86SYfOuDErFQ"
     },
     {
         title: "Google Cloud Certified",
