4ecb547c933ac651faeae3d0c5f9a07ba08cdce9
Anushka <anushkajais1001@gmail.com>
2025-08-01 09:07:17 +0000

    add a layout to cover the entire content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({
       <body className="font-sans antialiased bg-background p-1 bg-gradient-to-r from-black via-blue-900 to-white bg-[length:200%_100%] animate-gradient-border">
         <div className="bg-background">
           {children}
+          <Toaster />
         </div>
-        <Toaster />
       </body>
     </html>
   );
