e8885143333471566a364da2154f0f9ee5fd35c4
Anushka <anushkajais1001@gmail.com>
2025-08-18 17:23:36 +0000

    replace parallax scrolling with 'Scroll reveal animation'

--- src/components/layout/transition.tsx
+++ src/components/layout/transition.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useRef } from 'react';
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 
 type TransitionProps = {
   children: React.ReactNode;
@@ -10,18 +10,10 @@ type TransitionProps = {
 
 export default function Transition({ children }: TransitionProps) {
     const ref = useRef<HTMLDivElement>(null);
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["start end", "end start"]
-    });
-
-    // Adjust the output range to control the speed of the parallax effect
-    const y = useTransform(scrollYProgress, [0, 1], ["-15%", "15%"]);
 
     return (
         <div ref={ref} className="relative overflow-hidden">
             <motion.div
-                style={{ y }}
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true, amount: 0.1 }}
